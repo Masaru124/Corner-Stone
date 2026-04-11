ef85ef50df78d5a5852906c3ff1ce2e90b204bfc
Masaru124 <masarukaze041@gmail.com>
2026-04-11 16:37:54 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PWL0NKFD5B"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-PWL0NKFD5B');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${cormorant.variable} ${dmSans.variable} antialiased`}
       >
